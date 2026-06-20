90038a3db9761eb1f1271b29afb1ffdaf1803d29
nikkialenton <nikkialenton@gmail.com>
2026-06-21 04:16:25 +0800

    small adjustments

--- wedding-ui/src/environments/environment.prod.ts
+++ wedding-ui/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   adminKey: '5966885c-5770-4c5d-b6d0-293da327889c',
   siteUrl: 'https://arbieheartjerzey.netlify.app',
   adminUsers: [
-    { username: 'jerzey', password: 'jerzey1122!' },
-    { username: 'arbie', password: 'arbie1122!' },
+    { username: 'jerzey', password: 'jerzey01#' },
+    { username: 'arbie', password: 'arbie02@' },
     { username: 'support', password: 'support08!' }
   ]
 };
