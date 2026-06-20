f1d56dafffe063a1fbe9d598a77c252f26bfb818
nikkialenton <nikkialenton@gmail.com>
2026-06-21 04:06:22 +0800

    update scrollbar and environment vars

--- wedding-ui/src/environments/environment.prod.ts
+++ wedding-ui/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   adminKey: '5966885c-5770-4c5d-b6d0-293da327889c',
   siteUrl: 'https://arbieheartjerzey.netlify.app',
   adminUsers: [
-    { username: 'arbie', password: 'jerz_1100!' },
-    { username: 'jerzey', password: 'arbie_0011!' },
-    { username: 'support', password: 'admin_sup01' }
+    { username: 'jerzey', password: 'jerzey1122!' },
+    { username: 'arbie', password: 'arbie1122!' },
+    { username: 'support', password: 'support08!' }
   ]
 };
