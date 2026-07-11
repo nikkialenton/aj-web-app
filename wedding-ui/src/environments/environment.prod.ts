452e2fac1b25a1e0a59da9c63fcb3f00b980d4dc
nikkialenton <nikkialenton@gmail.com>
2026-07-11 09:33:29 +0800

    hero page revamp

--- wedding-ui/src/environments/environment.prod.ts
+++ wedding-ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://aj-web-app-production.up.railway.app/api',
   adminKey: '5966885c-5770-4c5d-b6d0-293da327889c',
-  siteUrl: 'https://arbieheartjerzey.netlify.app',
+  siteUrl: 'https://ajwedding.netlify.app',
   adminUsers: [
     { username: 'jerzey', password: 'jerzey01#' },
     { username: 'arbie', password: 'arbie02@' },
